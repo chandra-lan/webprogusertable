e38b9204dcbaf4aceb173b9d5197391d316a3f58
Chandra <hasukeyous@gmail.com>
2025-09-26 14:38:00 +0700

    User Table

---
+++ createdatabase.php
@@ -0,0 +1,13 @@
+<?php
+include 'connect.php';
+
+// Create database
+$sql = "CREATE DATABASE my5edb";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ createtable.php
@@ -0,0 +1,31 @@
+<?php
+
+include 'connect.php';
+
+// sql to create table
+// $sql = "CREATE TABLE MyGuests (
+// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+// firstname VARCHAR(30) NOT NULL,
+// lastname VARCHAR(30) NOT NULL,
+// email VARCHAR(50),
+// reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+// )";
+
+// sql to create table
+$sql = "CREATE TABLE user(
+id INT(11) NOT NULL AUTO_INCREMENT,
+username VARCHAR(60) NOT NULL,
+password VARCHAR(20) NOT NULL,
+fullname VARCHAR(60) NOT NULL,
+created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (id) 
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
